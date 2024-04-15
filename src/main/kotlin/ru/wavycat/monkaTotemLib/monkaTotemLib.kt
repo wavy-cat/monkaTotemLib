d6265d9d0def600f6a3299699dec61908a5dc7a1
wavy-cat <52543076+wavy-cat@users.noreply.github.com>
2024-04-15 21:41:43 +0500

    Refactor generate function in monkaTotemLib
    The generate function in the monkaTotemLib.kt file has been significantly refactored. The function now returns a Pair instead of a String. It also eliminates the conditional creation of processBuilder based on the roundHead value, and consolidates it into a single, coherent expression. Furthermore, the process output parsing has been improved to include a success indicator.

--- src/main/kotlin/ru/wavycat/monkaTotemLib/monkaTotemLib.kt
+++ src/main/kotlin/ru/wavycat/monkaTotemLib/monkaTotemLib.kt
@@ -21,39 +21,29 @@ class TotemBuilder(
         }
     }
 
-    fun generate(): String {
-        // Костыли. Фикс должен быть в https://github.com/wavy-cat/wavy-totem-lib/blob/main/cli.py
-        val processBuilder = when (roundHead) {
-            true -> ProcessBuilder(
-                pythonPath,
-                cliPath,
-                skinPath,
-                totemPath,
-                "--skin-type",
-                skinType.value,
-                "--top-layers",
-                topLayers.value,
-                "--round-head",
-                "true",
-                "--scale",
-                scale.toString()
-            )
-            false -> ProcessBuilder(
-                pythonPath,
-                cliPath,
-                skinPath,
-                totemPath,
-                "--skin-type",
-                skinType.value,
-                "--top-layers",
-                topLayers.value,
-                "--scale",
-                scale.toString()
-            )
-        }
+    /**
+     * Generates a totem using the CLI.
+     * Returns a pair of Boolean indicating whether the totem was generated successfully, and String containing the CLI output.
+     */
+    fun generate(): Pair<Boolean, String> {
+        val processBuilder = ProcessBuilder(
+            pythonPath,
+            cliPath,
+            skinPath,
+            totemPath,
+            "--skin-type",
+            skinType.value,
+            "--top-layers",
+            topLayers.value,
+            "--round-head",
+            roundHead.toString(),
+            "--scale",
+            scale.toString()
+        )
         val process = processBuilder.start()
         val reader = BufferedReader(InputStreamReader(process.inputStream))
-        val output = reader.readText()
-        return output
+        val output = reader.readText().removeSuffix("\n")
+        val ok = output == "\u001B[90m[\u001B[92m✔\u001B[90m] \u001B[94mGeneration completed successfully\u001B[0m"
+        return ok to output
     }
 }
